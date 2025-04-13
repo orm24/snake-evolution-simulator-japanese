775a925c5aab38263b053d955223170edf52056d
orm24 <ornst24m@mtholyoke.edu>
2025-04-13 01:54:25 -0400

    Update utils.js with Japanese comments

--- SnakeEvolutionSimulator/src/js/utils.js
+++ SnakeEvolutionSimulator/src/js/utils.js
@@ -1,28 +1,30 @@
 import { foods, snakes } from './global.js';
 
-// checks if position is occupied
+// （x、y）の位置が占領されているか確かめる関数
 export function isOccupied(x, y) {
-    // Check if any food is at this position
+    // 食べ物が占領しているか確かめる
     for (let f of foods) {
         if (f.xPosition === x && f.yPosition === y) {
             return true;
         }
     }
 
-    // Check all snakes
+    // ヘビが占領しているか確かめる
     for (let s of snakes) {
         if (s.xPosition === x && s.yPosition === y) {
             return true;
         }
     }
 
-    return false;
+    return false;　//占領されていない場合返り値false
 }
 
+//　a、bの選択肢のうちどちらかをランダムで選ぶ関数
 export function pickRandom(a, b) {
     return Math.random() < 0.5 ? a : b;
 }
 
+//　提示された最低値と最大値の間でランダムな数字を返す関数
 export function getRandomNumber(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
